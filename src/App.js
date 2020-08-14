bd7bd177862587dd4588e3c5afd59faf5cda0f19
sgneha <sgneha05@gmail.com>
2020-08-14 16:14:21 +0100

    different componentsname  and header components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">A slack clone</div>;
+  return (
+    // BEM naming convention
+    <div className="App">
+      <h1>A slack clone</h1>
+
+      {/* headers */}
+      {/* Sidebars */}
+      {/* React-routers -> chat screen */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Headers.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../Header.css";
+
+function Headers() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* Avatars for logged in users*/}
+        {/* Time*/}
+      </div>
+      ;
+      <div className="header__search">
+        {/*search icon */}
+        {/* input*/}
+      </div>
+      ;<div className="header-right">{/* help icon */}</div>;
+    </div>
+  );
+}
+
+export default Headers;
