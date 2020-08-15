b698663c2ec58e079f6294b40bceb8e657873f0a
sgneha <sgneha05@gmail.com>
2020-08-15 10:01:33 +0100

    sidebar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     // BEM naming convention
     <div className="App">
       <h1>A slack clone</h1>
-
-      {/* headers */}
       <Header />
-      {/* Sidebars */}
-      {/* React-routers -> chat screen */}
+      <div className="app__body">
+        <Sidebar />
+        {/* React-routers -> chat screen */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Sidebar.css";
+
+function Sidebar() {
+  return <div className="sidebar"></div>;
+}
+
+export default Sidebar;
