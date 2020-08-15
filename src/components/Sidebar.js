3f37f14a8656542dabf995b034dbf3a5aa6da80e
sgneha <sgneha05@gmail.com>
2020-08-15 10:18:46 +0100

    added sidebar header and info

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "../styles/Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
 
 function Sidebar() {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>slackclone</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Neha Singh
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
