f43f477f3d1193eccb82152fde5fe27d85382f7f
sgneha <sgneha05@gmail.com>
2020-08-16 13:51:47 +0100

    sidebar opions

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../styles/SidebarOption.css";
+
+function SidebarOption({ Icons, title }) {
+  //passing component Icon as props
+  return <div className="sidebarOption"></div>;
+}
+
+export default SidebarOption;
