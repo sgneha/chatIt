8243f8bb8e7e1ba0f51a00dc78ce94ab87bf5e7e
sgneha <sgneha05@gmail.com>
2020-08-16 14:13:59 +0100

    reusable component sidebaroption if icon passed then threads otherwise a channel is created with hash

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "../styles/SidebarOption.css";
 
-function SidebarOption({ Icons, title }) {
+function SidebarOption({ Icon, title }) {
   //passing component Icon as props
-  return <div className="sidebarOption"></div>;
+  return (
+    <div className="sidebarOption">
+      {Icon && <Icon className="sidebarOption__icon"></Icon>}
+      {Icon ? <h3>{title}</h3> : <h3>#{title}</h3>}
+    </div>
+  );
 }
 
 export default SidebarOption;
