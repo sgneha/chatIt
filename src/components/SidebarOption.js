a8aa78357677b0de6d2b990ea69a3192d2174604
sgneha <sgneha05@gmail.com>
2020-08-16 15:09:12 +0100

    sidebaroption css

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -6,7 +6,13 @@ function SidebarOption({ Icon, title }) {
   return (
     <div className="sidebarOption">
       {Icon && <Icon className="sidebarOption__icon"></Icon>}
-      {Icon ? <h3>{title}</h3> : <h3>#{title}</h3>}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className="sidebarOption__channel">
+          <span className="sidebarOption__hash">#</span> {title}
+        </h3>
+      )}
     </div>
   );
 }
