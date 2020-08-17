369042a28ff4cd66b318d5b7b677d3eaf31f81cd
sgneha <sgneha05@gmail.com>
2020-08-17 17:53:48 +0100

    all icons from material ui in chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/Chat.css";
 import { useParams } from "react-router-dom";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 
 function Chat() {
   const { roomId } = useParams(); //destructuring from the url
@@ -10,9 +12,14 @@ function Chat() {
         <div className="chat__headerLeft">
           <h2 className="chat__channelName">
             <strong>#general</strong>
+            <StarBorderOutlinedIcon />
           </h2>
         </div>
-        <div className="chat__headerRight"></div>
+        <div className="chat__headerRight">
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
       </div>
     </div>
   );
