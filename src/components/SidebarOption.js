def59ebd5b225bd279bbe33b19a9919d1351799d
sgneha <sgneha05@gmail.com>
2020-08-17 21:10:57 +0100

    selectChannel function  and uses useHistory hook

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../styles/SidebarOption.css";
+import { useHistory } from "react-router-dom";
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
   //passing component Icon as props
+  const history = useHistory(); //when back or forward a web page
+
+  const selectChannel = () => {
+    if (id) {
+      //pulls out the history
+      history.push(`/room/${id}`);
+    } else {
+      history.push(titile);
+    }
+  };
   return (
-    <div className="sidebarOption">
+    <div
+      className="sidebarOption"
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon className="sidebarOption__icon"></Icon>}
       {Icon ? (
         <h3>{title}</h3>
