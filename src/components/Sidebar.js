ca1bbab33d2b854873b6301e9fd9bb6d338a3729
sgneha <sgneha05@gmail.com>
2020-08-17 12:48:29 +0100

    sidebar completed,channels name is fetched from the db and shown on the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
@@ -13,8 +13,24 @@ import AppsIcon from "@material-ui/icons/Apps";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
+import db from "../firebase";
 
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+  useEffect(
+    () => {
+      db.collection("rooms").onSnapshot((snapshot) =>
+        setChannels(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            name: doc.data().name,
+          }))
+        )
+      );
+    },
+    [] //this is empty because we want it to rum just once when it loads
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -42,6 +58,9 @@ function Sidebar() {
 
       {/* Connect to DB and list all the channels */}
       {/* <SidebarOption /> */}
+      {channels.map((channel) => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
     </div>
   );
 }
--- src/process.md
+++ src/process.md
@@ -2,3 +2,5 @@ Installed firebase `sudo npm install -g firebase-tools`
 Created db in firebase and will connect now.
 This command will pull in all dependencies required for everything to be up and running.
 `npm i firebase`
+
+In Firestore collection has been set
