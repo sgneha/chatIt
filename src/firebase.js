d108fcc0ece429f587338a5caea8a5e227fa1cc7
sgneha <sgneha05@gmail.com>
2020-08-17 11:16:56 +0100

    frontend connected to database

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCLgZl9ZEWt51nA-t8D51M_ZIBKdN02yb4",
   authDomain: "chatit-43675.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:594867593820:web:cf3ef9e5f39db2d125fc99",
   measurementId: "G-MT3CYYX5C9",
 };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
