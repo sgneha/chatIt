a328c237866003cf8036cf546a68771257ee6ccf
sgneha <sgneha05@gmail.com>
2020-08-17 09:16:10 +0100

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCLgZl9ZEWt51nA-t8D51M_ZIBKdN02yb4",
+  authDomain: "chatit-43675.firebaseapp.com",
+  databaseURL: "https://chatit-43675.firebaseio.com",
+  projectId: "chatit-43675",
+  storageBucket: "chatit-43675.appspot.com",
+  messagingSenderId: "594867593820",
+  appId: "1:594867593820:web:cf3ef9e5f39db2d125fc99",
+  measurementId: "G-MT3CYYX5C9",
+};
---
+++ src/process.md
@@ -0,0 +1,4 @@
+Installed firebase `sudo npm install -g firebase-tools`
+Created db in firebase and will connect now.
+This command will pull in all dependencies required for everything to be up and running.
+`npm i firebase`
